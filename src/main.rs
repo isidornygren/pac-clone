a5b3c4157a40151e4b20570e92dbd69d426f79bd
Isidor Nygren <karlisidornygren@gmail.com>
2018-12-12 20:36:44 +0000

    Added basic ggez example code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+extern crate ggez;
+use ggez::graphics::{DrawMode, Point2};
+use ggez::*;
+
+struct MainState {
+    pos_x: f32,
+}
+
+impl MainState {
+    fn new(_ctx: &mut Context) -> GameResult<MainState>{
+        let state = MainState{ pos_x: 0.0_f32 };
+        Ok(state)
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        graphics::circle(ctx,
+                         DrawMode::Fill,
+                         Point2::new(self.pos_x, 380.0),
+                         100.0,
+                         2.0)?;
+        graphics::present(ctx);
+        Ok(())
+    }
+}
+
+pub fn main() {
+    let context_builder = ContextBuilder::new("pacman_clone", "ggez")
+        .window_setup(conf::WindowSetup::default().title("Pacman Clone!"))
+        .window_mode(conf::WindowMode::default().dimensions(224, 288));
+
+    let ctx = &mut context_builder.build().unwrap();
+    let state = &mut MainState::new(ctx).unwrap();
+    event::run(ctx, state).unwrap();
 }
